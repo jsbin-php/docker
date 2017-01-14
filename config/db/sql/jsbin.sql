289fc065f38b3a8e599f4367c748d7e2ef20e8cf
MarQuis Knox <MarQuisKnox@users.noreply.github.com>
2017-01-14 17:41:06 +0100

    Create jsbin.sql

---
+++ config/db/sql/jsbin.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS jsbin;
+CREATE DATABASE IF NOT EXISTS jsbin;
+USE jsbin;
+
+CREATE TABLE IF NOT EXISTS `sandbox` (
+  `id` int(11) NOT NULL auto_increment,
+  `javascript` text character set utf8,
+  `html` text character set utf8,
+  `created` datetime default NULL,
+  `last_viewed` datetime default NULL,
+  `url` char(255) default NULL,
+  `streaming` char(1) default 'n',
+  `streaming_key` char(32) NOT NULL,
+  `streaming_read_key` char(32) NOT NULL,
+  `active_tab` varchar(10) NOT NULL,
+  `active_cursor` int(11) NOT NULL,
+
+  `revision` int(11) default 1,
+
+  PRIMARY KEY  (`id`),
+  KEY `viewed` (`last_viewed`),
+  KEY `url` (`url`),
+  KEY `streaming_key` (`streaming_key`),
+  KEY `spam` (`created`,`last_viewed`),
+  KEY `revision` (`url`, `revision`)
+) character set utf8;
+
+CREATE TABLE IF NOT EXISTS `owners` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` char(255) NOT NULL,
+  `url` char(255) NOT NULL,
+  `revision` int(11) default 1,
+  
+  PRIMARY KEY  (`id`),
+  KEY `name_url` (`name`, `url`, `revision`)
+);
